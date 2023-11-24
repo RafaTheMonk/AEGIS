42d261aeb2e27d542a451429a66a2c88b51917cc
Rafael <rafaelbritovaladares@gmail.com>
2023-11-24 06:11:43 -0300

    Base para inserção de dados com método post

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 import express from 'express'
 const app = express()
 
+//Indicar para o express ler body com json
+app.use(express.json())
+//mock
+const selecoes = [
+    {id: 1, selecao: 'Brazil', grupo: 'G'},
+    {id: 2, selecao: 'England', grupo: 'G'},
+    {id: 3, selecao: 'Camaroes', grupo: 'G'},
+]
 
 app.get('/', (req, res) => {
   res.send('Helo World!')
+})
+
+app.get('/seguranca', (req, res) => {
+  res.status(200).send(selecoes)
+})
+
+app.post('/seguranca', (req, res) => {
+  selecoes.push(req.body)
+  res.status(201).send('Seleção cadastrada com sucesso!')
 })
 
 
