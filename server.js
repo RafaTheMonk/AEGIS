9f367386d90accb2e52828d965458a9af96389de
Rafael <rafaelbritovaladares@gmail.com>
2023-11-25 16:26:32 -0300

    Versão 1.0

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 import app from './src/App.js'
 
-import conexao from './infra/conexao.js'
-
 const PORT = 3000
 
-// Fazer a conexão
-conexao.connect((erro)=>{
-    if(erro){
-        console.log(`Conexão falhou ${erro}`)
-    }else{
-        console.log('Conexão realizado com sucesso!')
-        // Escutar a porta 3000
-        app.listen(PORT, () => {
-            console.log(`Servidor Rodando na porta ${PORT}`)
-        })
-    }
+// Escutar a porta 3000
+app.listen(PORT, () => {
+    console.log(`Servidor Rodando na porta ${PORT}`)
 })
 
