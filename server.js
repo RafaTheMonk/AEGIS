ac65fdfd77731fe855f6a4ab62b1838eeeb768cc
Rafael <rafaelbritovaladares@gmail.com>
2023-11-25 14:31:24 -0300

    instalação e conexão do servidor mysql

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 import app from './src/App.js'
+
+import conexao from './infra/conexao.js'
+
 const PORT = 3000
 
-app.listen(PORT, () => {
-    console.log(`Servidor Rodando ${PORT}`)
+// Fazer a conexão
+conexao.connect((erro)=>{
+    if(erro){
+        console.log(`Conexão falhou ${erro}`)
+    }else{
+        console.log('Conexão realizado com sucesso!')
+        // Escutar a porta 3000
+        app.listen(PORT, () => {
+            console.log(`Servidor Rodando na porta ${PORT}`)
+        })
+    }
 })
+
