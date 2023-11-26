939563e04b02001f827449c345f3b4ea5d075c08
Rafael <rafaelbritovaladares@gmail.com>
2023-11-26 09:39:55 -0300

    Organização das rotas no arquivo routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 import express from 'express';
-import UsuariosController from './app/controllers/UsuariosController.js';
+import routes from './routes.js';
+
 const app = express();
 
 app.use(express.json());
 
-app.post('/usuarios', UsuariosController.store);
-app.get('/usuarios', UsuariosController.index);
-app.get('/usuarios/:id', UsuariosController.show);
-app.put('/usuarios/:id', UsuariosController.update);
-app.delete('/usuarios/:id', UsuariosController.delete);
+// Usar o router
+app.use(routes)
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import app from './App.js'
 
-const PORT = 3000
+// A porta passada pelo serviço de hospedagem ou a porta 3000
+const PORT = process.env.PORT || 3000
 
 // Escutar a porta 3000
 app.listen(PORT, () => {
