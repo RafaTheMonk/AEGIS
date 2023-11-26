68057f19dfe7265c0797e81a19e0c8464763af27
Rafael <rafaelbritovaladares@gmail.com>
2023-11-26 09:44:10 -0300

    gitignore

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import app from './App.js'
 // A porta passada pelo serviço de hospedagem ou a porta 3000
 const PORT = process.env.PORT || 3000
 
-// Escutar a porta 3000
+// Escutar a porta
 app.listen(PORT, () => {
     console.log(`Servidor Rodando na porta ${PORT}`)
 })
