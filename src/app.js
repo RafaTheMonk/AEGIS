81ee72ab2bb6a42cd43a9a489b4b1f8bd82cd479
Rafael <rafaelbritovaladares@gmail.com>
2024-01-08 04:03:28 -0300

    corse

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import routes from './routes.js'
+import cors from 'cors';
 
 const app = express()
 
+app.use(cors());
 // indicar para o express ler body com json
 app.use(express.json())
 
